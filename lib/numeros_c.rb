80fbd418fb1c469851501d05baa279c503a75d5b
Juan Tareq <juantareq@MacBook-Pro-de-Juan-2.local>
2014-10-14 21:17:37 +0100

    Agregada funcion del operador + para Fixnum y 2 complejos

--- lib/numeros_c.rb
+++ lib/numeros_c.rb
@@ -8,6 +8,17 @@ class Numero_c
 	def to_s
 		"(#{real},#{imag}i)"
 	end
-	
+
+	def +(other)
+		if (other.instance_of? Fixnum)
+			return Numero_c.new((@real+other),@imag)
+		end
+
+		if (other.instance_of? Numero_c)
+			return Numero_c.new(@real+other.real,@imag+other.imag)
+		else
+			puts "Error, imposible sumar"
+		end
+	end
 end
 
--- test/tc_numeros_c.rb
+++ test/tc_numeros_c.rb
@@ -8,4 +8,12 @@ class Test_NumeroComplejo < Test::Unit::TestCase
 		@num = Numero_c.new(4,5)
 		assert_equal("(4,5i)",@num.to_s)
 	end
+
+	def test_suma
+		@num = Numero_c.new(4,5)
+		@num2 = Numero_c.new(5,2)
+		assert_equal("(6,5i)",(@num+2).to_s,"Error suma con Fixnum")
+		assert_equal("(9,7i)",(@num+@num2).to_s,"Error suma complejos")
+	end
+
 end
