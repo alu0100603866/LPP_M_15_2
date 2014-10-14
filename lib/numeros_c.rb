4bced5fff71fea096ff13b613e227335a3291ce3
Juan Tareq <juantareq@MacBook-Pro-de-Juan-2.local>
2014-10-14 22:27:24 +0100

    Agregada la division y su test, y conversion de los numeros a flotantes para mejor funcionamiento de la division

--- lib/numeros_c.rb
+++ lib/numeros_c.rb
@@ -2,8 +2,8 @@ class Numero_c
 	attr_reader :real, :imag
     
 	def initialize(r,i)
-		@real = r
-		@imag = i
+		@real = r.to_f
+		@imag = i.to_f
 	end
 
 	def to_s
@@ -41,6 +41,18 @@ class Numero_c
             return Numero_c.new(@real*other.real-@imag*other.imag,@real*other.imag+@imag*other.real)
         else
             puts "Error, imposible multiplicar"
+        end
+    end
+
+    def /(other)
+        if (other.instance_of? Fixnum)
+            return Numero_c.new(@real/other,@imag/other)
+        end
+        if (other.instance_of? Numero_c)
+            denom = other.real*other.real+other.imag*other.imag
+            return Numero_c.new((@real*other.real+@imag*other.imag)/denom,(@imag*other.real-@real*other.imag)/denom)
+        else
+            puts "Error, imposible dividir"
         end
     end
 end
--- test/tc_numeros_c.rb
+++ test/tc_numeros_c.rb
@@ -5,28 +5,34 @@ require "test/unit"
 class Test_NumeroComplejo < Test::Unit::TestCase
 
     def setup
-        @num = Numero_c.new(4,5)
+        @num = Numero_c.new(4,6)
         @num2 = Numero_c.new(5,2)
+        @numD = Numero_c.new(2,1)
 	end
     
     def test_ini
 		@num = Numero_c.new(4,5)
-		assert_equal("(4,5i)",@num.to_s)
+		assert_equal("(4.0,5.0i)",@num.to_s)
 	end
 
 	def test_suma
 		
-		assert_equal("(6,5i)",(@num+2).to_s,"Error suma con Fixnum")
-		assert_equal("(9,7i)",(@num+@num2).to_s,"Error suma complejos")
+		assert_equal("(6.0,6.0i)",(@num+2).to_s,"Error suma con Fixnum")
+		assert_equal("(9.0,8.0i)",(@num+@num2).to_s,"Error suma complejos")
 	end
     
     def test_resta
-        assert_equal("(2,5i)",(@num-2).to_s,"Error suma con Fixnum")
-        assert_equal("(-1,3i)",(@num-@num2).to_s,"Error suma complejos")
+        assert_equal("(2.0,6.0i)",(@num-2).to_s,"Error suma con Fixnum")
+        assert_equal("(-1.0,4.0i)",(@num-@num2).to_s,"Error suma complejos")
     end
 
     def test_multiplicacion
-	    assert_equal("(8,10i)",(@num*2).to_s,"Error multiplicar por Fixnum")
-	    assert_equal("(10,33i)",(@num*@num2).to_s,"Error multiplicacion complejos")
+	    assert_equal("(8.0,12.0i)",(@num*2).to_s,"Error multiplicar por Fixnum")
+	    assert_equal("(8.0,38.0i)",(@num*@num2).to_s,"Error multiplicacion complejos")
+    end
+    
+    def test_division
+        assert_equal("(2.0,3.0i)",(@num/2).to_s,"Error multiplicar por Fixnum")
+        assert_equal("(2.8,1.6i)",(@num/@numD).to_s,"Error division complejos")
     end
 end
